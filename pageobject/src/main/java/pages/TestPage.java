0af494b31a4317c11dfd3f9563283488522261f2
84Engineer <alx84@ukr.net>
2016-08-27 13:27:52 +0300

    initial project structure

---
+++ pageobject/src/main/java/pages/TestPage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ *
+ * @author oslysenko
+ */
+public class TestPage {
+    
+    public void go() {
+        String driverPath = "D:/IEDriver32/IEDriverServer.exe";
+        System.setProperty("webdriver.ie.driver", driverPath);
+        WebDriver driver = new InternetExplorerDriver();
+        
+        driver.navigate().to("http://google.com");
+    }
+    
+}
